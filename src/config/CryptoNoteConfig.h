41101f5f6247e601305479d9c983356cf824b55a
Zent Cash <46155884+Zentcash@users.noreply.github.com>
2019-02-27 19:20:27 +0100

    version 1.2.2
    Fix mixin issues.

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -92,7 +92,7 @@ const uint64_t MINIMUM_FEE                                   = UINT64_C(10);
 const uint64_t MINIMUM_MIXIN_V1                              = 0;
 const uint64_t MAXIMUM_MIXIN_V1                              = 0;
 
-const uint64_t MINIMUM_MIXIN_V2                              = 3;
+const uint64_t MINIMUM_MIXIN_V2                              = 0;
 const uint64_t MAXIMUM_MIXIN_V2                              = 3;
 
 const uint64_t MINIMUM_MIXIN_V3                              = 3;
@@ -100,8 +100,8 @@ const uint64_t MAXIMUM_MIXIN_V3                              = 3;
 
 /* The heights to activate the mixin limits at */
 const uint32_t MIXIN_LIMITS_V1_HEIGHT                        = 0;
-const uint32_t MIXIN_LIMITS_V2_HEIGHT                        = 5;
-const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 6;
+const uint32_t MIXIN_LIMITS_V2_HEIGHT                        = 11000;
+const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 150000;
 
 /* The mixin to use by default with zedwallet and turtle-service */
 /* DEFAULT_MIXIN_V0 is the mixin used before MIXIN_LIMITS_V1_HEIGHT is started */
@@ -256,7 +256,7 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 };
 
 const char* const SEED_NODES[] = {
-  "95.216.169.11:21688",//rock
+  "95.216.169.11:21688", //rock
   "95.216.191.56:21688", //cision
 
 };
